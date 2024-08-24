aec02d5767562c687b17cf89d7ec16f70a9d3ef1
heyjaja <imjaja09@gmail.com>
2024-08-24 17:58:33 +0900

    ch2. 도메인 제외 삭제

--- src/main/java/com/architecture/hexagonal/ch2/application/port/output/RouterViewOutputPort.java
+++
@@ -1,9 +0,0 @@
-package com.architecture.hexagonal.ch2.application.port.output;
-
-import com.architecture.hexagonal.ch2.domain.entity.Router;
-
-import java.util.List;
-
-public interface RouterViewOutputPort {
-    List<Router> fetchRouters();
-}
